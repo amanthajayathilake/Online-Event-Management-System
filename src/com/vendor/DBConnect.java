936c94c41aa8ad4e6e654c580e98bdd657862c99
Amantha Jayathilake <79798311+amanthajayathilake@users.noreply.github.com>
2021-09-17 00:00:12 +0530

    Update DBConnect.java

--- src/com/vendor/DBConnect.java
+++ src/com/vendor/DBConnect.java
@@ -7,7 +7,7 @@ public class DBConnect {
 	
 	private static String url = "jdbc:mysql://localhost:3306/eventdb";
 	private static String userName = "root";
-	private static String password = "romanreigns";
+	private static String password = "";
 	private static Connection con;
 
 	public static Connection getConnection() {
